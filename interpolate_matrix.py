7849de8938e449b3a70b7a5677ea20b112421caf
Chris Bird <chrisjbird@gmail.com>
2020-06-04 04:54:24 +0100

    solves unit tests except for one

--- interpolate_matrix.py
+++ interpolate_matrix.py
@@ -2,3 +2,9 @@
 
 def interpolate_matrix(matrix):
     pass
+
+
+def interpolate_element(values):
+    available_values = [x for x in values if x is not None]
+    # print(f'available_values: {available_values}')
+    return round(sum(available_values) / len(available_values), 6)
