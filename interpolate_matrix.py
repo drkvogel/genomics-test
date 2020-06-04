7705091b85a0cf2995e08f779babe37e11957b98
Chris Bird <chrisjbird@gmail.com>
2020-06-04 04:59:46 +0100

    dev: print some stuff

--- interpolate_matrix.py
+++ interpolate_matrix.py
@@ -1,8 +1,15 @@
 
 
 def interpolate_matrix(matrix):
-    pass
-
+    # pass
+    for row in matrix:
+        print(f'row: {row}')
+        for col in row:
+            if col == 'nan':
+                print('nan!!')
+            else:
+                print(f'col: {col}')
+    # print(matrix)
 
 def interpolate_element(values):
     available_values = [x for x in values if x is not None]
