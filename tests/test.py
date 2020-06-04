b31ae63de58acfdc0f43bbf3a7d3377faf1ef856
Chris Bird <chrisjbird@gmail.com>
2020-06-04 03:24:19 +0100

    autopep8

--- tests/test.py
+++ tests/test.py
@@ -2,23 +2,28 @@ import unittest
 import csv
 from interpolate_matrix import interpolate_matrix
 
+
 class TestInterpolateMatrix(unittest.TestCase):
 
     def setUp(self):
         self.input_test_data_1 = []
         with open('./example_data/input_test_data.csv') as f:
-          reader = csv.reader(f)
-          for row in reader:
-            self.input_test_data_1.append([row[0], row[1], row[2], row[3], row[4]])
+            reader = csv.reader(f)
+            for row in reader:
+                self.input_test_data_1.append(
+                    [row[0], row[1], row[2], row[3], row[4]])
 
         self.interpolated_test_data_1 = []
         with open('./example_data/interpolated_test_data.csv') as f:
-          reader = csv.reader(f)
-          for row in reader:
-            self.interpolated_test_data_1.append([row[0], row[1], row[2], row[3], row[4]])
+            reader = csv.reader(f)
+            for row in reader:
+                self.interpolated_test_data_1.append(
+                    [row[0], row[1], row[2], row[3], row[4]])
 
     def test_interpolate_input_test_data_1(self):
-        self.assertEqual(interpolate_matrix(self.input_test_data_1), self.interpolated_test_data_1)
+        self.assertEqual(interpolate_matrix(
+            self.input_test_data_1), self.interpolated_test_data_1)
+
 
 if __name__ == '__main__':
     unittest.main()
