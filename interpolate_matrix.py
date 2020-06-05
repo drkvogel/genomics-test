20e0acee1268f1ea2c925f5b18e8967a9e19d170
Chris Bird <chrisjbird@gmail.com>
2020-06-05 16:01:45 +0100

    fix logic for args to interpolated_matrix()

--- interpolate_matrix.py
+++ interpolate_matrix.py
@@ -21,8 +21,8 @@ def interpolated_matrix(matrix):
                 # North East South West
                 interpolated = interpolate_element([
                     float(matrix[y - 1][x]) if y > 0 and matrix[y - 1][x] is not NAN_STRING else None, # N
-                    float(matrix[y][x + 1]) if x + 1 < w - 1 and matrix[y][x + 1] is not NAN_STRING else None, # E
-                    float(matrix[y + 1][x]) if y + 1 < h - 1 and matrix[y + 1][x] is not NAN_STRING else None, # S
+                    float(matrix[y][x + 1]) if x + 1 <= w - 1 and matrix[y][x + 1] is not NAN_STRING else None, # E
+                    float(matrix[y + 1][x]) if y + 1 <= h - 1 and matrix[y + 1][x] is not NAN_STRING else None, # S
                     float(matrix[y][x - 1]) if x - 1 >= 0 and matrix[y][x - 1] is not NAN_STRING else None, # W
                 ])
                 print(f'interpolated: {interpolated}\n')
